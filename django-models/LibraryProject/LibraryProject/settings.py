9b26ecb737f0e5d170a54db40b143bc8ffed7f54
yazzy01 <rziguiyassir@gmail.com>
2024-11-13 11:56:32 -0800

    Fix: Ensure register.html template is correctly referenced

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Ensure this includes the root templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -62,5 +62,5 @@ def register(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
 
