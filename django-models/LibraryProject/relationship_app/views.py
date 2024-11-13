524cfd4f362336317281dd34a624de7432ac90c1
yazzy01 <rziguiyassir@gmail.com>
2024-11-13 10:00:00 -0800

    Fix import issue: Added missing import for Library model

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
 from .models import Author, Book, Library, Librarian
+from django.views.generic.detail import DetailView
+
 
 # Existing views
 def author_list(request):
