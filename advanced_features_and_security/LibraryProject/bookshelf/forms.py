15fca0655ed2173b57fc901db34a70ded419a18c
yazzy01 <rziguiyassir@gmail.com>
2024-11-22 07:16:29 -0800

    Implemented security best practices: settings, CSRF, ORM, and CSP

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'description', 'published_date']
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
 
