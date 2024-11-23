d4880bc3f7d1403c92c9fd6fa0e6cc450026c441
yazzy01 <rziguiyassir@gmail.com>
2024-11-23 10:01:53 -0800

    Add BookList API endpoint with serializer, view, and URL configuration

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 class BookList(ListAPIView):
-    queryset = Book.objects.all()  # Query all Book instances
-    serializer_class = BookSerializer  # Use the BookSerializer for data serialization
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('api/', include('api.urls')),  # Include URLs from the api app
 ]
 
+
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/', include('api.urls')),
+]
+
