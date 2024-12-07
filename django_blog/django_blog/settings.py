a7aa3453760c99a12b58e1d85284b80915a94bbb
yazzy01 <rziguiyassir@gmail.com>
2024-12-07 11:05:11 -0800

    Fix database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',  # Empty string
+        'PORT': '',  # Empty string
     }
 }
 
