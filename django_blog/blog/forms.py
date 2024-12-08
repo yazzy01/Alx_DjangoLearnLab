e6b651d96122c1dbef5bb3d359401123f237b1cf
yazzy01 <rziguiyassir@gmail.com>
2024-12-08 15:40:28 -0800

    Implemented tagging and search functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.forms import TagWidget  # Import the TagWidget
+from taggit.forms import TagWidget
+from django.forms import widgets  # Explicitly import widgets
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Enter a valid email address.")
@@ -17,6 +18,9 @@ class PostForm(forms.ModelForm):
         fields = ['title', 'content', 'tags']  # Include 'tags' field
 
     tags = forms.CharField(widget=TagWidget(), required=False)  # Add the TagWidget to the 'tags' field
+
+    # Explicitly using widgets if needed
+    content = forms.CharField(widget=widgets.Textarea(attrs={'rows': 4, 'cols': 40}))  # For content field
 
 class CommentForm(forms.ModelForm):
     class Meta:
