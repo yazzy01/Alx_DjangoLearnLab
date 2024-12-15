0ab90e1dd3cc7c42493dde049a9bd11f87a8325a
yazzy01 <rziguiyassir@gmail.com>
2024-12-15 13:59:04 -0800

    Fix URL recursion issue and update accounts URLs

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,27 +13,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        Token.objects.create(user=user)
-        return user
-
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
 # Get the custom user model
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    # Explicitly define fields with serializers.CharField
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    email = serializers.EmailField()
+    username = serializers.CharField(max_length=150)  # Explicitly using serializers.CharField
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})  # Explicitly using serializers.CharField
+    email = serializers.EmailField()  # Email field remains the same
 
     class Meta:
         model = User
@@ -49,4 +37,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         # Automatically create an auth token for the new user
         Token.objects.create(user=user)
         return user
-
