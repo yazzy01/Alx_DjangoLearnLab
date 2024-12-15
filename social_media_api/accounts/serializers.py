53f0ee99ea0daa2eb46726da92ef2bfea1d86440
yazzy01 <rziguiyassir@gmail.com>
2024-12-15 13:50:07 -0800

    Fix URL recursion issue and update accounts URLs

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -22,3 +22,31 @@ class RegisterSerializer(serializers.ModelSerializer):
         Token.objects.create(user=user)
         return user
 
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+# Get the custom user model
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # Explicitly define fields with serializers.CharField
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        # Use get_user_model().objects.create_user for user creation
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Automatically create an auth token for the new user
+        Token.objects.create(user=user)
+        return user
+
