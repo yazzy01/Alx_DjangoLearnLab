167e74cd1a6628a3c045a06197e740753ea1c293
yazzy01 <rziguiyassir@gmail.com>
2024-12-15 14:24:23 -0800

    Fix URL recursion issue and update accounts URLs

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,52 +1,23 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
-from rest_framework.validators import UniqueValidator
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        validators=[validate_password]
-    )
-    password2 = serializers.CharField(
-        write_only=True,
-        required=True
-    )
+# Get the custom user model
+User = get_user_model()
 
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+class RegisterSerializer(serializers.Serializer):  # Use `Serializer` instead of `ModelSerializer`
+    username = serializers.CharField(max_length=150)  # Explicitly using CharField
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})  # Explicitly using CharField
+    email = serializers.EmailField()  # Email field remains unchanged
 
     def create(self, validated_data):
-        user = User.objects.create(
+        # Use get_user_model().objects.create_user for user creation
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            password=validated_data['password']
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # Automatically create an auth token for the new user
+        Token.objects.create(user=user)
         return user
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
